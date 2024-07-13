1dd0283ca8ac344593360d1242416b79d15ad394
mohamed lazaar <lazzarmohamed10@gmail.com>
2024-07-13 16:12:57 +0100

    completing the simple quiz task

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+    // Function body
+
+    const correctAnswer= "4";
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;    console.log(userAnswer)
+    let feedback = document.getElementById('feedback');
+    if(correctAnswer === userAnswer){
+        
+        feedback.textContent = "Correct! Well done.";
+    }else{
+        feedback.textContent ="That's incorrect. Try again!"
+    }
+}
+
+let btn = document.getElementById('submit-answer');
+btn.addEventListener('click', checkAnswer)
